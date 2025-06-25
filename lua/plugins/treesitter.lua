f2cc3c8e9e269dc26e36181cb68750113a795f93
jounger <nguyenvanan.12a8@gmail.com>
2025-06-26 03:07:58 +0700

    add: treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,41 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            highlight = {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = 'master',
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                highlight = {
+                    enable = true,
+                },
+                indent = { enable = true },
+                autotage = { enable = true },
+                auto_install = false,
+                sync_install = false,
+                ignore_install = {},
+                ensure_installed = {
+                    "lua",
+                    "tsx",
+                    "typescript",
+                    "javascript",
+                    "go",
+                    "python",
+                },
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context").setup {
+                mode = 'topline',
                 enable = true,
-            },
-            indent = { enable = true },
-            autotage = { enable = true },
-            auto_install = false,
-            ensure_installed = {
-                "vim",
-                "lua",
-                "tsx",
-                "typescript",
-                "javascript",
-                "go",
-                "gomod",
-                "python",
-            },
-        })
-    end
+                multiwindow = true,
+                max_lines = 0,
+                line_numbers = true,
+            }
+        end
+    },
 }
