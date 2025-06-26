3c350bec5f8fbc723f06bb3a6ac157ef49cb0c97
jounger <nguyenvanan.12a8@gmail.com>
2025-06-26 11:43:03 +0700

    add: undotree

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,6 +1,16 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 
+-- remap control-c to work same as Esc
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- move up or down and then center
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
 -- yank paragraph to clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- reload without exiting vim
+vim.keymap.set("n", "<leader>rl", "<cmd>source ~/.config/nvim/init.lua<cr>")
--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -8,4 +8,13 @@ return {
             require('mini.surround').setup()
         end
     },
+    {
+        "mbbill/undotree",
+        config = function()
+            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+            vim.g.undotree_WindowLayout = 3
+            vim.g.undotree_SplitWidth = 50
+            vim.g.undotree_DiffAutoOpen = 1
+        end
+    },
 }
